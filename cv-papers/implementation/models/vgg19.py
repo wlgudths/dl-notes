1bef768e80078fb3598a9ef625f2bb0f776304b6
손지형 <sonjeehyung@sonjihyeong-ui-MacBookPro.local>
2025-07-15 17:22:43 +0900

    Feat: vgg16, vgg19 issue: #3

---
+++ cv-papers/implementation/models/vgg19.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # block1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+        )
+        
+        # block2
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, 1, 1)
+        )
+
+        # block3
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+        )
+        # block4
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+        )
+        # block5
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+        )
+        
+        # classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 1000)
+        )
+
+        # Maxpool
+        self.maxpool = nn.MaxPool2d(2, 2)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.maxpool(x)
+        x = self.conv3(x)
+        x = self.maxpool(x)
+        x = self.conv4(x)
+        x = self.maxpool(x)
+        x = self.conv5(x)
+        x = self.maxpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    vgg19 = VGG19()
+    summary(vgg19, (1, 3, 224, 224))
